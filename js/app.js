0b9842c4624d5a5df964c33b838438c1e1d5ffc2
Peter Masucci <peter.a.masucci@gmail.com>
2015-05-10 15:22:19 -0400

    Set up game logic and win conditions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,61 @@
-$($('.square').click(function(){
-$( this ).text('X');
-})
-)
+$(document).ready(function() {
+
+    var players = ['X', 'O'];
+    var player = 0;
+
+    $('.square').click(function() {
+
+        var square = $(this);
+
+        if (square.attr('value') === undefined) {
+
+            $(this).text(players[player]);
+            $(this).attr('value', players[player]);
+            if(getWinner()){
+              alert("Player " + players[player] + " has won!");
+            }
+
+            player = 1 - player;
+        }
+
+    });
+
+function getWinner() {
+    var board = [];
+    var ret = false;
+    console.log("inside getWinner")
+
+    $('.square').each(function() {
+        board.push($(this).attr('value'));
+    });
+
+    for (var i = 0; i < players.length; i++) {
+        if (players[player] === board[0] && players[player] === board[1] && players[player] === board[2]) {
+            ret = true;
+            console.log("first win condition")
+        } else if (players[player] === board[3] && players[player] === board[4] && players[player] === board[5]) {
+            ret = true;
+        } else if (players[player] === board[6] && players[player] === board[7] && players[player] === board[8]) {
+            ret = true;
+        } else if (players[player] === board[0] && players[player] === board[3] && players[player] === board[6]) {
+            ret = true;
+        } else if (players[player] === board[1] && players[player] === board[4] && players[player] === board[7]) {
+            ret = true;
+        } else if (players[player] === board[2] && players[player] === board[5] && players[player] === board[8]) {
+            ret = true;
+        } else if (players[player] === board[0] && players[player] === board[4] && players[player] === board[8]) {
+            ret = true;
+        } else if (players[player] === board[3] && players[player] === board[5] && players[player] === board[6]) {
+            ret = true;
+        }
+    }
+
+    return ret;
+
+}
+
+
+});
+
+
+
