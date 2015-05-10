f9eb8387c6fa388902967e0b583becf5f0dcd48c
Peter Masucci <peter.a.masucci@gmail.com>
2015-05-10 16:00:17 -0400

    Add tie game detection

--- js/app.js
+++ js/app.js
@@ -2,17 +2,21 @@ $(document).ready(function() {
 
     var players = ['X', 'O'];
     var player = 0;
+    var gameState = {};
 
     $('.square').click(function() {
 
         var square = $(this);
 
-        if (square.attr('value') === undefined) {
+        if (square.attr('value') === undefined && !gameState.gameOver) {
 
             $(this).text(players[player]);
             $(this).attr('value', players[player]);
-            if(getWinner()){
-              alert("Player " + players[player] + " has won!");
+            gameState = getWinner();
+            if (gameState.gameOver && gameState.winner) {
+                alert("Player " + gameState.winner + " has won!");
+            } else if (gameState.gameOver) {
+                alert("The game has ended in a tie!");
             }
 
             player = 1 - player;
@@ -20,42 +24,57 @@ $(document).ready(function() {
 
     });
 
-function getWinner() {
-    var board = [];
-    var ret = false;
-    console.log("inside getWinner")
+    function getWinner() {
+        var board = [];
+        var ret = false;
+        var winner;
+        $('.square').each(function() {
+            board.push($(this).attr('value'));
+        });
+        var turnCount = board.filter(function(i) {
+            return i !== undefined;
+        }).length
 
-    $('.square').each(function() {
-        board.push($(this).attr('value'));
-    });
 
-    for (var i = 0; i < players.length; i++) {
-        if (players[player] === board[0] && players[player] === board[1] && players[player] === board[2]) {
-            ret = true;
-            console.log("first win condition")
-        } else if (players[player] === board[3] && players[player] === board[4] && players[player] === board[5]) {
-            ret = true;
-        } else if (players[player] === board[6] && players[player] === board[7] && players[player] === board[8]) {
-            ret = true;
-        } else if (players[player] === board[0] && players[player] === board[3] && players[player] === board[6]) {
-            ret = true;
-        } else if (players[player] === board[1] && players[player] === board[4] && players[player] === board[7]) {
-            ret = true;
-        } else if (players[player] === board[2] && players[player] === board[5] && players[player] === board[8]) {
-            ret = true;
-        } else if (players[player] === board[0] && players[player] === board[4] && players[player] === board[8]) {
-            ret = true;
-        } else if (players[player] === board[3] && players[player] === board[5] && players[player] === board[6]) {
-            ret = true;
+
+        for (var i = 0; i < players.length; i++) {
+            if (players[player] === board[0] && players[player] === board[1] && players[player] === board[2]) {
+                ret = true;
+                winner = players[player];
+            } else if (players[player] === board[3] && players[player] === board[4] && players[player] === board[5]) {
+                ret = true;
+                winner = players[player];
+            } else if (players[player] === board[6] && players[player] === board[7] && players[player] === board[8]) {
+                ret = true;
+                winner = players[player];
+            } else if (players[player] === board[0] && players[player] === board[3] && players[player] === board[6]) {
+                ret = true;
+                winner = players[player];
+            } else if (players[player] === board[1] && players[player] === board[4] && players[player] === board[7]) {
+                ret = true;
+                winner = players[player];
+            } else if (players[player] === board[2] && players[player] === board[5] && players[player] === board[8]) {
+                ret = true;
+                winner = players[player];
+            } else if (players[player] === board[0] && players[player] === board[4] && players[player] === board[8]) {
+                ret = true;
+                winner = players[player];
+            } else if (players[player] === board[2] && players[player] === board[4] && players[player] === board[6]) {
+                ret = true;
+                winner = players[player];
+            }
+
+            if (!ret && turnCount === 9) {
+                ret = true;
+            }
         }
+
+        return {
+            gameOver: ret,
+            winner: winner
+        };
+
     }
-
-    return ret;
-
-}
 
 
 });
-
-
-
