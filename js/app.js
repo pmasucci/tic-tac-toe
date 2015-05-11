16182d66fa952d59974f977c5f3b6d5747ced6e8
Peter Masucci <peter.a.masucci@gmail.com>
2015-05-10 20:00:59 -0400

    Sick ass animations added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     var players = ['X', 'O'];
+    var playerSVG = ['<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" width="69.247864mm" height="69.247864mm" viewBox="0 0 245.36645 245.36645" version="1.1"><style>.s0{fill:none;stroke-width:15.63562679;stroke:#000;}</style><g transform="translate(0,-806.99577)"><path transform="matrix(0.70710678,-0.70710678,0.70710678,0.70710678,0,0)" d="m-572.14056 587.77625 2.08173 0 0 312.71252-2.08173 0zM-745.1734-727.45593l2.08174 0 0 312.71252-2.08174 0" class="s0"/><path transform="matrix(-0.70710678,-0.70710678,0.70710678,-0.70710678,0,0)" d="m-745.1734-727.45593 2.08174 0 0 312.71252-2.08174 0z" class="s0"/></g></svg>', '<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" width="69.247864mm" height="69.247856mm" viewBox="0 0 245.36644 245.36641" version="1.1"><g transform="translate(-296.79263,-260.4939)"><path d="M535.27095 383.17734A115.7953 115.7953 0 0 1 419.47565 498.97264 115.7953 115.7953 0 0 1 303.68034 383.17734 115.7953 115.7953 0 0 1 419.47565 267.38203 115.7953 115.7953 0 0 1 535.27095 383.17734Z" style="fill:none;stroke-miterlimit:4;stroke-width:13.77540779;stroke:#000"/></g></svg>']
     var player = 0;
     var gameState = {};
 
@@ -10,13 +11,16 @@ $(document).ready(function() {
 
         if (square.attr('value') === undefined && !gameState.gameOver) {
 
-            $(this).text(players[player]);
+            $(this).html(playerSVG[player]);
+            var svg = new Walkway('#' + $(this).attr('id'));
+            svg.draw();
+            setTimeout(function(){$('path').css('stroke-dasharray', '0')}, 300);
             $(this).attr('value', players[player]);
             gameState = getWinner();
             if (gameState.gameOver && gameState.winner) {
-                alert("Player " + gameState.winner + " has won!");
+                setTimeout(function(){alert("Player " + gameState.winner + " has won!")}, 600);
             } else if (gameState.gameOver) {
-                alert("The game has ended in a tie!");
+                setTimeout(function(){alert("The game has ended in a tie!")}, 600);
             }
 
             player = 1 - player;
